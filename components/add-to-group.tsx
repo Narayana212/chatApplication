8cffe2ec6f6eca74d63c1eb36e57d6e0c3c34419
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-08-29 11:35:34 +0530

    first commit

--- components/add-to-group.tsx
+++ components/add-to-group.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import React, { FC, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -9,12 +9,73 @@ import {
   DialogTrigger,
 } from "./ui/dialog";
 import { Button } from "./ui/button";
-import { Plus } from "lucide-react";
+import { Loader2, Plus, PlusIcon, ShieldCloseIcon, X } from "lucide-react";
 import { Input } from "./ui/input";
+import { useToast } from "./ui/use-toast";
+import { ChatState } from "@/context/chat-provider";
+import { Badge } from "./ui/badge";
 
 interface AddToGroupProps {}
 
 const AddToGroup: FC<AddToGroupProps> = () => {
+  const [groupChatName, setGroupChatName] = useState<string>("");
+  const [selectedUsers, setSelectedUsers] = useState<any[]>([]);
+  const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const { toast } = useToast();
+  const { user, chats, setChats } = ChatState();
+
+  async function handleSearch(query: string) {
+    setSearch(query);
+    if (!query) {
+      return;
+    }
+    try {
+      setLoading(true);
+      const response = await fetch(`api/users/search?search=${query}`);
+      const data = await response.json();
+      if (response.ok) {
+        setSearchResult(data.message);
+      } else {
+        toast({
+          title: data.message,
+        });
+      }
+    } catch (error: any) {
+      toast({
+        title: error.message,
+      });
+    }
+    setLoading(false);
+  }
+
+  function handleGroup(user) {
+    if (selectedUsers.includes(user)) {
+      toast({
+        title: "User Already added",
+      });
+      return;
+    }
+    setSelectedUsers([...selectedUsers, user]);
+  }
+
+  function handleUserRemove(user: any) {
+    setSelectedUsers((prevUsers) =>
+      prevUsers.filter((u) => u._id !== user._id)
+    );
+  }
+
+  async function handleSubmit() {
+    if (!groupChatName || selectedUsers.length === 0) {
+      toast({
+        title: "Please fill all details",
+      });
+    } else {
+      // Perform the actual chat creation logic here
+    }
+  }
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -27,12 +88,42 @@ const AddToGroup: FC<AddToGroupProps> = () => {
         <DialogHeader>
           <DialogTitle>Create a Group Chat</DialogTitle>
           <DialogDescription>
-            <Input placeholder="Your Group Chat Name" className="mt-2" />
-            <Input placeholder="Add users" className="mt-3" />
+            <Input
+              placeholder="Your Group Chat Name"
+              className="mt-2"
+              value={groupChatName}
+              onChange={(e) => setGroupChatName(e.target.value)}
+            />
+            <Input
+              placeholder="Add users"
+              className="mt-3"
+              value={search}
+              onChange={(e) => handleSearch(e.target.value)}
+            />
+            <div className="w-full flex flex-wrap mt-3">
+              {selectedUsers.map((user) => ( 
+                <Badge key={user._id} className="m-1 p-3">
+                  {user.name} <X onClick={() => handleUserRemove(user)} className="h-4 pl-2 cursor-pointer" />
+                </Badge>
+              ))}
+            </div>
+            {loading ? (
+              <Loader2 className="animate-spin" />
+            ) : (searchResult?.slice(0, 4).map((user) => (
+                <Badge
+                  className="m-1 mt-3 p-4 "
+                  key={user._id}
+                  variant={"outline"}
+                >
+                  {user.name}
+                  <PlusIcon onClick={() => handleGroup(user)} className="h-4 pl-2 cursor-pointer" />
+                </Badge>
+              ))
+            )}
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button>Create the Chat</Button>
+          <Button onClick={handleSubmit}>Create the Chat</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
