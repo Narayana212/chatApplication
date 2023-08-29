d09111a2c1c1820c22d25a0c22f6e38b8cbe336e
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-08-30 01:08:49 +0530

    first commit

--- helpers/get-data-from-token.ts
+++ helpers/get-data-from-token.ts
@@ -1,12 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
-export const getDataFromToken=(request:NextRequest)=>{
-    try{
-        const jwtToken=request.cookies.get("token")?.value || ""
-        const decodedToken=jwt.verify(jwtToken,"that_is_secret")
-        return decodedToken
-    }catch(error:any){
-        console.log(error.message)
+export const getDataFromToken = (request: NextRequest) => {
+    try {
+        const jwtToken = request.cookies.get("token")?.value || "";
+        const decodedToken = jwt.verify(jwtToken, "that_is_secret");
+        return decodedToken;
+    } catch (error: any) {
+        console.log("Failed to decode token:", error.message);
+        return null;
     }
-}
+};
